74f9046714e78a8c3c7afbf700f4fc2ce78fb7f6
derins <ds5bb@virginia.edu>
2019-10-22 16:58:18 -0700

    comments added

--- scripts/main.js
+++ scripts/main.js
@@ -29,15 +29,20 @@ function sortPlots() {
     var filter_type = sort_options[0];
 
 
+    // accesses the manually updated json file on s3 bucket
     $.getJSON("https://s3-us-west-1.amazonaws.com/lctk.data/derin-test/2_plots_list.json", function(plots) {
         if ((x_axis != y_axis)) {
+          // collects all the objects with the corresponding filter type in array - filtered
             for (var plot = 0; plot < plots.length; plot++) {
                 if ((plots[plot][filter_type] == filter) && (plots[plot].normalization == normalization)) {
                     filtered.push(plots[plot]);
                 }
             }
+            // sorts array acvcording to y-axis input
             filtered.sort((a, b) => (a[y_axis] > b[y_axis]) ? 1 : -1);
+            //creates new arrays - rows - of objects with unique y-axis input values
             rows.push(filtered[0]);
+            // adds all the rows in to the final array - xy_sort - which is an array of arrays of objects
             for (var plot = 0; plot < filtered.length - 1; plot++) {
                 if (filtered[plot][y_axis] == filtered[plot + 1][y_axis]) {
                     rows.push(filtered[plot + 1]);
@@ -47,9 +52,11 @@ function sortPlots() {
                 }
             }
             xy_sort.push(rows);
+            // sorts all the inner arrays of xy_sort according to x_axis input
             for (var array = 0; array < xy_sort.length; array++) {
                 xy_sort[array].sort((a, b) => (a[x_axis] > b[x_axis]) ? 1 : -1);
             }
+            // outputs everything in to html page in a grid format
             $("#content").empty();
             for (var x = 0; x < xy_sort.length; x++) {
                 $("#content").append("<div id='rows' class='row" + x + "'>");
@@ -61,9 +68,9 @@ function sortPlots() {
                 }
                 $("#content").append("</div>");
             }
+        // error message
         } else {
           $("#content").empty().append("<h2>please select a different value for each dropdown and make sure each drop down has a selected value</h2>")
         }
-
     });
 }
